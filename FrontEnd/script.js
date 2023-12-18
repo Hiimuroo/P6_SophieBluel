3c3005f6e5f26456443aa354db0c4bea5a526a39
Hiimuroo <matthieu.labille@gmail.com>
2023-12-18 17:40:48 +0100

    modified:   FrontEnd/assets/style.css modified:   FrontEnd/index.html modified:   Modale ajoutPhoto

--- FrontEnd/script.js
+++ FrontEnd/script.js
@@ -1,90 +1,92 @@
 document.addEventListener('DOMContentLoaded', () => {
-    const galleryContainer = document.getElementById('items');
-    const apiUrl = 'http://localhost:5678/api/works';
-    
-    document.getElementById('btnTous').addEventListener('click', function() {
-      changeButtonState(this);
-    });
-    
-    document.getElementById('btnObjets').addEventListener('click', function() {
-      changeButtonState(this);
-    });
-    
-    document.getElementById('btnAppartements').addEventListener('click', function() {
-      changeButtonState(this);
-    });
-    
-    document.getElementById('btnHotelsrestaurants').addEventListener('click', function() {
-      changeButtonState(this);
-    });
+  const galleryContainer = document.getElementById('galeries');
+  const apiUrl = 'http://localhost:5678/api/works';
 
-    let activeButton = null;
+  document.getElementById('btnTous').addEventListener('click', function () {
+    changeButtonState(this);
+  });
 
-    function changeButtonState(button) {
-      if (activeButton) {
-        activeButton.classList.remove('active');
+  document.getElementById('btnObjets').addEventListener('click', function () {
+    changeButtonState(this);
+  });
+
+  document.getElementById('btnAppartements').addEventListener('click', function () {
+    changeButtonState(this);
+  });
+
+  document.getElementById('btnHotelsrestaurants').addEventListener('click', function () {
+    changeButtonState(this);
+  });
+
+  let activeButton = null;
+
+  function changeButtonState(button) {
+    if (activeButton) {
+      activeButton.classList.remove('active');
+    }
+
+    button.classList.add('active');
+    activeButton = button;
+  }
+
+  let allItems = [];
+
+  fetch(apiUrl)
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Internet indisponible');
       }
-    
-      button.classList.add('active');
-      activeButton = button;
-    }
-    
+      return response.json();
+    })
+    .then(data => {
+      allItems = data;
+      updateUIWithData(data);
+    })
+    .catch(error => {
+      console.error('Erreur lors de la récupération des données:', error);
+    });
+
+  function updateUIWithData(data) {
+    galleryContainer.innerHTML = '';
+    galleryContainer.classList.add('gallery');
+
+    data.forEach(item => {
+      const galleryItem = createGalleryItem(item);
+      galleryContainer.appendChild(galleryItem);
+    });
+  }
+
+  function createGalleryItem(item, isModal = false) {
+    const galleryItem = document.createElement('div');
   
-    let allItems = [];
-  
-    fetch(apiUrl)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Internet indisponible');
-        }
-        return response.json();
-      })
-      .then(data => {
-        allItems = data;
-        updateUIWithData(data);
-      })
-      .catch(error => {
-        console.error('Erreur lors de la récupération des données:', error);
-      });
-  
-    function updateUIWithData(data) {
-      galleryContainer.innerHTML = '';
-      galleryContainer.classList.add('gallery');
-  
-      data.forEach(item => {
-        const galleryItem = createGalleryItem(item);
-        galleryContainer.appendChild(galleryItem);
-      });
-    }
-  
-    function createGalleryItem(item) {
-      const galleryItem = document.createElement('div');
+    if (isModal) {
       galleryItem.classList.add('modal-gallery-item');
-  
-      const image = document.createElement('img');
-      image.src = item.imageUrl;
-      image.alt = item.title;
-      galleryItem.appendChild(image);
-  
-      const title = document.createElement('figcaption');
-      title.textContent = item.title;
-      galleryItem.appendChild(title);
-  
-      return galleryItem;
+    } else {
+      galleryItem.classList.add('gallery-item');
     }
   
-    function filterItems(categoryId) {
-      const filteredItems = categoryId === 'all' ? allItems : allItems.filter(item => item.categoryId === categoryId);
-      updateUIWithData(filteredItems);
-    }
+    const image = document.createElement('img');
+    image.src = item.imageUrl;
+    image.alt = item.title;
+    galleryItem.appendChild(image);
   
-    document.getElementById('btnTous').addEventListener('click', () => filterItems('all'));
-    document.getElementById('btnObjets').addEventListener('click', () => filterItems(1));
-    document.getElementById('btnAppartements').addEventListener('click', () => filterItems(2));
-    document.getElementById('btnHotelsrestaurants').addEventListener('click', () => filterItems(3));
+    const title = document.createElement('figcaption');
+    title.textContent = item.title;
+    galleryItem.appendChild(title);
+  
+    return galleryItem;
+  }
 
+  function filterItems(categoryId) {
+    const filteredItems = categoryId === 'all' ? allItems : allItems.filter(item => item.categoryId === categoryId);
+    updateUIWithData(filteredItems);
+  }
+
+  document.getElementById('btnTous').addEventListener('click', () => filterItems('all'));
+  document.getElementById('btnObjets').addEventListener('click', () => filterItems(1));
+  document.getElementById('btnAppartements').addEventListener('click', () => filterItems(2));
+  document.getElementById('btnHotelsrestaurants').addEventListener('click', () => filterItems(3));
 });
-
 
 document.addEventListener('DOMContentLoaded', () => {
   const loginButton = document.getElementById('loginBtn');
@@ -231,8 +233,8 @@ document.addEventListener('DOMContentLoaded', () => {
       imageElement.alt = `Photo ID: ${photo.id}`;
 
       const deleteIcon = document.createElement('span');
-      deleteIcon.classList.add('delete-icon');
-      deleteIcon.innerHTML = '&#128465;';
+      deleteIcon.innerHTML = '<i class="fas fa-trash"></i>';
+      deleteIcon.classList.add('delete');
       deleteIcon.onclick = () => supprimerImage(photo.id, galleryItem);
 
       galleryItem.appendChild(imageElement);
@@ -261,4 +263,63 @@ function supprimerImage(imageId, galleryItem) {
       })
       .catch(error => console.error('Erreur réseau :', error));
   }
+}
+
+function ajouterPhoto(photoFile) {
+  const formData = new FormData();
+  formData.append('photo', photoFile);
+
+  fetch('http://localhost:5678/api/works', {
+    method: 'POST',
+    body: formData,
+  })
+    .then(response => {
+      if (response.ok) {
+        alert('Photo ajoutée avec succès!');
+        chargerPhotos();
+      } else {
+        throw Error('Erreur lors de l\'ajout de la photo.');
+      }
+    })
+    .catch(error => console.error('Erreur réseau :', error));
+}
+
+function chargerPageAjoutPhoto() {
+  const modalHeader = document.querySelector('.modal-header');
+  modalHeader.innerHTML = '';
+
+  const ajoutPhotoContent = `
+  <div class="arrow-container" onclick="retourGalerie()">
+    <i class="fa-solid fa-arrow-left"></i>
+  </div>
+  <h2>Ajout photo</h2>
+  <div class="contents">
+    <form id="ajoutPhotoForm">
+      <div class="photo-container">
+        <i class="fa-regular fa-image"></i>
+        <input type="file" accept=".jpg, .jpeg, .png" id="photoInput" style="display: none;" onchange="handleFileSelect(event)">
+        <label for="photoInput" class="photo-button">+ Ajouter photo</label>
+        <p>jpg, png : 4mo max</p>
+      </div>
+    </form>
+    <h3>Titre</h3>
+    <input type="text" id="titleInput" required>
+    <h3>Catégorie</h3>
+    <div class="form-group">
+      <select id="categorySelect" class="form-control" required>
+        <option value="" selected disabled></option>
+        <option value="1">Objets</option>
+        <option value="2">Appartements</option>
+        <option value="3">Hotels & restaurants</option>
+      </select>
+    </div>
+    <hr>
+  </div>
+`;
+  modalHeader.insertAdjacentHTML('beforeend', ajoutPhotoContent);
+}
+
+function getTitreProjet() {
+  const titreInput = document.getElementById('titleInput');
+  return titreInput.value;
 }
